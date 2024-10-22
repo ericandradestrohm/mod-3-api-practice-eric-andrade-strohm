74c95ca5be8352b8e60f0d2c9adcc5b4d833c06d
Eric <ericandradestrohmbvt2023@gmail.com>
2024-10-21 19:29:34 -0700

    Created async function for fetching cat image. Function also appends image to HTML. Finished styling.

--- app.js
+++ app.js
@@ -1,16 +1,36 @@
+const mainDiv = document.querySelector('.container');
+const mainButton = document.querySelector('button');
+const newDiv = document.createElement('div');
+const catImg = document.createElement('img');
 const apiUrl = 'https://api.thecatapi.com/v1/';
+
+newDiv.classList.add('image');
+catImg.id = "cat-image";
 
 async function catFetch() {
     fetch(`${apiUrl}images/search`, {
         method: 'get'
-    }).then(response => {
-        console.log(response);
-        response.json().then(data => {
-            console.log(data);
-        }).catch(err => {
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+            // Assign image source to cat url
+            catImg.src = data[0].url;
+
+            // Append cat image and button to new div
+            newDiv.appendChild(catImg);
+            mainButton.textContent = 'Click here for another cat!';
+            newDiv.appendChild(mainButton);
+            
+            // Append new div to parent div
+            mainDiv.appendChild(newDiv);
+        })
+        .catch(err => {
             console.log(err);
         })
-    }).catch(err => {
+        .catch(err => {
         console.log(err)
     })
 }
+
+mainButton.addEventListener('click', catFetch);
